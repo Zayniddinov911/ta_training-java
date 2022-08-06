486725a515b6a58f4f287f417a722a1534a71e86
Ibrat Zayniddinov <tony@Ibrats-Mac-mini.local>
2022-08-06 10:59:15 +0500

    changes

--- ta_training-java/src/test/java/com/epam/training/student_Ibrat_Zayniddinov/HelloAutoCode.java
+++
@@ -1,8 +0,0 @@
-package com.epam.training.student_Ibrat_Zayniddinov;
-
-public class HelloAutoCode {
-    public static void main(String[] args) {
-        System.out.println("Hello, AutoCode");
-    }
-}
-
