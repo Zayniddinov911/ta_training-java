92a8933244a31c78a3ee4f441d16deda545efd05
Ibrat Zayniddinov <tony@Ibrats-Mac-mini.local>
2022-08-07 18:28:20 +0500

    next program

---
+++ ta_training-java/src/main/java/com/epam/training/student_Ibrat_Zayniddinov/InputStringProgram.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_Ibrat_Zayniddinov;
+
+
+import java.util.Scanner;
+
+public class InputStringProgram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = (int) scan.nextDouble();
+        System.out.println(number);
+
+    }
+}
