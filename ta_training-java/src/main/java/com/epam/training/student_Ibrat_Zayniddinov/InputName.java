e44ca1d0a4aa0f7eb47ce8add876cbe5ae9ac145
Ibrat Zayniddinov <tony@Ibrats-Mac-mini.local>
2022-08-07 09:04:56 +0500

    new class

---
+++ ta_training-java/src/main/java/com/epam/training/student_Ibrat_Zayniddinov/InputName.java
@@ -0,0 +1,12 @@
+package com.epam.training.student_Ibrat_Zayniddinov;
+
+
+import java.util.Scanner;
+
+public class InputName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Hello, " + input);
+    }
+}
